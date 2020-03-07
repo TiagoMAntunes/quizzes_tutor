e7c3498e68721086184e4d04a531580d0b9f1ac5
pedro-lamego <pedrownlamego@hotmail.com>
2020-03-07 16:01:40 +0000

    define tests started

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/StudentCreateSubmitQuestionTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/StudentCreateSubmitQuestionTest.groovy
@@ -1,83 +1,79 @@
-package pt.ulisboa.tecnico.socialsoftware.tutor.question.service
+//package pt.ulisboa.tecnico.socialsoftware.tutor.question.service
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import spock.lang.Specification
 
 @DataJpaTest
-class StudentCreateQuestionTest extends Specification {
+class StudentCreateSubmitQuestionTest extends Specification {
 
+    def questionService
 
-    def ""(){
-        //
+    /*def setup() {
+        questionService = new QuestionService()
+    }
+*/
+    def "the course and topic exist and title and body of text is valid "() {
+        // the question is created or submited
         expect: false
     }
 
-    def ""(){
-        //
-        expect: false
-    }
-
-    def ""(){
-        //
-        expect: false
-    }
-
-    def ""(){
-        //
-        expect: false
-    }
-
-    def "title is blank"(){
+    def "not a student"() {
         // an exception is thrown
         expect: false
     }
 
-    def "title is empty"(){
+    def "title already exists"() {
         // an exception is thrown
         expect: false
     }
 
-    def "body of text is blank"(){
+    def "title is blank"() {
         // an exception is thrown
         expect: false
     }
 
-    def "body of text is empty"(){
+    def "title is empty"() {
         // an exception is thrown
         expect: false
     }
 
-    def "course is empty"(){
+    def "body of text is blank"() {
         // an exception is thrown
         expect: false
     }
 
-    def "course is blank"(){
+    def "body of text is empty"() {
         // an exception is thrown
         expect: false
     }
 
-    def "course doesnt exist"(){
+    def "course is empty"() {
         // an exception is thrown
         expect: false
     }
 
-    def "topic is empty"(){
+    def "course is blank"() {
         // an exception is thrown
         expect: false
     }
 
-    def "topic is blank"(){
+    def "course doesnt exist"() {
         // an exception is thrown
         expect: false
     }
 
-    def "topic doesnt exist"(){
+    def "topic is empty"() {
         // an exception is thrown
         expect: false
     }
 
+    def "topic is blank"() {
+        // an exception is thrown
+        expect: false
+    }
 
-
-
+    def "topic doesnt exist"() {
+        // an exception is thrown
+        expect: false
+    }
 }
