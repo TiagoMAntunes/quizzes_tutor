c121cea8f322f72c62480520703d799f3e81457d
pedro-lamego <pedrownlamego@hotmail.com>
2020-03-07 16:01:20 +0000

    define tests started

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/StudentCreateSubmitQuestionTest.groovy
@@ -0,0 +1,83 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.service
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import spock.lang.Specification
+
+@DataJpaTest
+class StudentCreateQuestionTest extends Specification {
+
+
+    def ""(){
+        //
+        expect: false
+    }
+
+    def ""(){
+        //
+        expect: false
+    }
+
+    def ""(){
+        //
+        expect: false
+    }
+
+    def ""(){
+        //
+        expect: false
+    }
+
+    def "title is blank"(){
+        // an exception is thrown
+        expect: false
+    }
+
+    def "title is empty"(){
+        // an exception is thrown
+        expect: false
+    }
+
+    def "body of text is blank"(){
+        // an exception is thrown
+        expect: false
+    }
+
+    def "body of text is empty"(){
+        // an exception is thrown
+        expect: false
+    }
+
+    def "course is empty"(){
+        // an exception is thrown
+        expect: false
+    }
+
+    def "course is blank"(){
+        // an exception is thrown
+        expect: false
+    }
+
+    def "course doesnt exist"(){
+        // an exception is thrown
+        expect: false
+    }
+
+    def "topic is empty"(){
+        // an exception is thrown
+        expect: false
+    }
+
+    def "topic is blank"(){
+        // an exception is thrown
+        expect: false
+    }
+
+    def "topic doesnt exist"(){
+        // an exception is thrown
+        expect: false
+    }
+
+
+
+
+}
