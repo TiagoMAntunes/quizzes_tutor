0c0c3f2d89d5536cb87d7113713787e3ca43386c
pedro-lamego <pedrownlamego@hotmail.com>
2020-03-08 00:57:38 +0000

    Implementing f1.1 tests started

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/StudentCreateSubmitQuestionTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/StudentCreateSubmitQuestionTest.groovy
@@ -1,13 +1,40 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.question.service
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.QuestionService
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.TopicRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserRepository
 import spock.lang.Specification
 
 @DataJpaTest
 class StudentCreateSubmitQuestionTest extends Specification {
+    public static final String COURSE_NAME = "Software Architecture"
+    public static final String INVALID_COURSE_NAME = "Invalid_Course"
+    public static final String TITLE_NAME = "Main_Title"
+    public static final String TOPIC_NAME = "Main_Topic"
+    public static final String INVALID_TOPIC_NAME = "Invalid_Topic"
+    public static final String BODY_TEXT = "Main_Body_Text"
+    public static final String BLANK = "    "
 
-    def questionService
+
+    @Autowired
+    UserRepository userRepository
+
+    @Autowired
+    CourseRepository courseRepository
+
+    @Autowired
+    TopicRepository topicRepository
+
+    def student
+    def course
+    def topic
+    def bodyText
 
     def setup() {
         questionService = new QuestionService()
@@ -18,7 +45,7 @@ class StudentCreateSubmitQuestionTest extends Specification {
         expect: false
     }
 
-    def "not a student"() {
+    def "not the student that create the question"() {
         // an exception is thrown
         expect: false
     }
