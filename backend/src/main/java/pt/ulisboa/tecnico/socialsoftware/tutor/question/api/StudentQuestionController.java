45b2226d16958d4445b6e54a32f07a319147bf35
apollee <mariabeatriz.dongamartins@gmail.com>
2020-03-21 23:18:27 +0000

    Defined web service. Issue #71

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/api/StudentQuestionController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/api/StudentQuestionController.java
@@ -2,11 +2,22 @@ package pt.ulisboa.tecnico.socialsoftware.tutor.question.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.StudentQuestionService;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.StudentQuestion;
+
+import java.util.List;
 
 @RestController
 public class StudentQuestionController {
+
     @Autowired
     private StudentQuestionService  studentQuestionService;
 
+    @GetMapping("/questions/{studentId}")
+    @PreAuthorize("hasRole('ROLE_STUDENT') and hasPermission(#questionId, 'QUESTION.ACCESS')")
+    public List<StudentQuestion> getAllStudentQuestions(@PathVariable int studentId){
+        return this.studentQuestionService.getStudentQuestions(studentId);
+    }
 }
