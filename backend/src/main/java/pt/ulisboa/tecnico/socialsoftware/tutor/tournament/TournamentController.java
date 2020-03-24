2d90490a01826dea0d1ffe40b92d3026a9f8c4dc
AfonsoRibeiro <1998afonsoribeiro1998@gmail.com>
2020-03-24 11:15:37 +0000

    Defined cancel tournament web service Closes #76

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentController.java
@@ -1,6 +1,7 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.tournament;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -22,9 +23,6 @@ public class TournamentController {
     @Autowired
     private TournamentService tournamentService;
 
-    @Autowired
-    private UserRepository userRepository;
-
     @PostMapping("/executions/{executionId}/tournaments")
     @PreAuthorize("hasRole('ROLE_STUDENT') and hasPermission(#executionId, 'EXECUTION.ACCESS')")
     public TournamentDto createTournament(@PathVariable int executionId, @Valid @RequestBody TournamentDto tournament, Principal principal) {
@@ -35,6 +33,20 @@ public class TournamentController {
         }
 
         return this.tournamentService.createTournament(tournament, executionId, user.getId());
+    }
+
+    @DeleteMapping("/tournaments/{tournamentId}")
+    @PreAuthorize("hasRole('ROLE_STUDENT') and hasPermission(#tournamentId, 'TOURNAMENT.CANCEL')")
+    public ResponseEntity deleteTournament(@PathVariable Integer tournamentId, Principal principal) {
+        User user = (User) ((Authentication) principal).getPrincipal();
+
+        if (user == null) {
+            throw new TutorException(AUTHENTICATION_ERROR);
+        }
+
+        tournamentService.cancelTournament(tournamentId, user.getId());
+
+        return ResponseEntity.ok().build();
     }
 
 }
