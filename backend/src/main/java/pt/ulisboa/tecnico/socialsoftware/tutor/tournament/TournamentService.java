1675ad05faf393cb27095ea8f2576b6dbe74dd1c
Tiago Antunes <tiago.melo.antunes@gmail.com>
2020-03-24 12:17:07 +0000

    Merge branch 'tdp' into afonsoribeiro/f4.1/web_service (PR #83)

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/tournament/TournamentService.java
@@ -63,9 +63,6 @@ public class TournamentService {
         backoff = @Backoff(delay = 5000))
     @Transactional(isolation = Isolation.REPEATABLE_READ)
     public TournamentDto createTournament(TournamentDto tournamentDto, int courseExecutionId, int creatorId) {
-        if (tournamentDto.getKey() == null)
-            tournamentDto.setKey(getMaxTournamentKey() + 1);
-
         if (tournamentDto.getTopics() == null)
             throw new TutorException(ErrorMessage.NO_TOPICS_SELECTED);
 
@@ -189,10 +186,5 @@ public class TournamentService {
     private Tournament getTournament(Integer tournamentId) {
         return tournamentRepository.findById(tournamentId)
                 .orElseThrow(() -> new TutorException(ErrorMessage.TOURNAMENT_NOT_FOUND, tournamentId));
-    }
-
-    private Integer getMaxTournamentKey() {
-        Integer val = tournamentRepository.getMaxTournamentKey();
-        return val != null ? val : 0;
     }
 }
