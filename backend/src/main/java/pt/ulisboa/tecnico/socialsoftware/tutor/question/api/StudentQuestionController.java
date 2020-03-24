eb5ff10aaafbfa6151c232f74d6c9362b87109dd
pedro-lamego <pedrownlamego@hotmail.com>
2020-03-24 18:40:32 +0000

    Defined web service. closes #80

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/api/StudentQuestionController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/api/StudentQuestionController.java
@@ -1,12 +1,38 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.question.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.StudentQuestionService;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.StudentQuestionDto;
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User;
+
+import static pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage.*;
+
+import javax.validation.Valid;
+import java.security.Principal;
 
 @RestController
 public class StudentQuestionController {
+
     @Autowired
     private StudentQuestionService  studentQuestionService;
 
+    @PostMapping("/courses/{courseId}/questions")
+    @PreAuthorize("hasRole('ROLE_STUDENT') and hasPermission(#courseId, 'COURSE.ACCESS')")
+    public StudentQuestionDto createStudentQuestion(@PathVariable int courseId, @Valid @RequestBody QuestionDto question, Principal principal) {
+        question.setStatus(Question.Status.AVAILABLE.name());
+        User user = (User) ((Authentication) principal).getPrincipal();
+        if(user == null){
+            throw new TutorException(AUTHENTICATION_ERROR);
+        }
+        return this.studentQuestionService.createStudentQuestion(courseId, question, user.getId());
+    }
 }
