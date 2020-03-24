23f491728569328f72ab8d45f3c3ce4682722aa2
apollee <mariabeatriz.dongamartins@gmail.com>
2020-03-24 19:09:51 +0000

    Defined functionality test of the web service. Closes #72

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/api/StudentQuestionController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/question/api/StudentQuestionController.java
@@ -8,17 +8,26 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.question.StudentQuestionService;
 import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.StudentQuestion;
 
 import javax.validation.Valid;
+import java.io.Serializable;
 
 @RestController
-public class StudentQuestionController {
+public class StudentQuestionController implements Serializable{
+
+    static class Information{
+        public String status;
+        public String explanation;
+        public int teacherId;
+        public int courseExecutionId;
+    }
 
     @Autowired
     private StudentQuestionService  studentQuestionService;
 
-    @PostMapping("/questions/{questionId}/approve-reject")
-    @PreAuthorize("hasRole('ROLE_TEACHER') and hasPermission(#questionId, 'QUESTION.ACCESS')")
-    public ResponseEntity studentQuestionApproveRejectStatus(@PathVariable Integer questionId, @Valid @RequestBody String status, @Valid @RequestBody String explanation, @Valid @RequestBody int teacherId, @Valid @RequestBody int courseExecutionId) {
-        studentQuestionService.studentQuestionApproveReject(questionId, StudentQuestion.QuestionStatus.valueOf(status), explanation, teacherId,  courseExecutionId);
+    @PostMapping("/student_questions/{questionId}/approve-reject")
+    @PreAuthorize("hasRole('ROLE_TEACHER')")
+    public ResponseEntity studentQuestionApproveRejectStatus(@PathVariable Integer questionId, @Valid @RequestBody Information inform) {
+        studentQuestionService.studentQuestionApproveReject(questionId, StudentQuestion.QuestionStatus.valueOf(inform.status), inform.explanation, inform.teacherId,  inform.courseExecutionId);
         return ResponseEntity.ok().build();
     }
 }
+
