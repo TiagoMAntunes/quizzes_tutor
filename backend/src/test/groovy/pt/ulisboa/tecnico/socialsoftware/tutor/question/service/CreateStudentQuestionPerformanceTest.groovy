19ef8b6d45cfffbc3419f3c83e1d053fdf6a6d14
pedro-lamego <pedrownlamego@hotmail.com>
2020-03-26 16:33:04 +0000

    Finished proffiling #93

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/CreateStudentQuestionPerformanceTest.groovy
@@ -0,0 +1,97 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.Course
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecution
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseExecutionRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.course.CourseRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.StudentQuestionService
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.OptionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.StudentQuestionRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.repository.TopicRepository
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.User
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.UserRepository
+import spock.lang.Specification
+
+
+
+@DataJpaTest
+class CreateStudentQuestionPerformanceTest extends Specification {
+    public static final String COURSE_NAME = "Software Architecture"
+    public static final String ACRONYM = "AS1"
+    public static final String ACADEMIC_TERM = "1 SEM"
+    public static final String QUESTION_TITLE = 'question title'
+    public static final String QUESTION_CONTENT = 'question content'
+    public static final String OPTION_CONTENT = "optionId content"
+    public static final String USER_NAME = "pedro"
+    public static final String USER_USERNAME = "lamegow"
+    public static final int N_STUDENT_QUESTIONS   = 100
+
+    @Autowired
+    CourseRepository courseRepository
+
+    @Autowired
+    CourseExecutionRepository courseExecutionRepository
+
+    @Autowired
+    StudentQuestionRepository studentQuestionRepository
+
+    @Autowired
+    UserRepository userRepository
+
+    @Autowired
+    StudentQuestionService studentQuestionService
+
+    def course
+    def courseExecution
+    def student
+
+    def setup() {
+        course = new Course(COURSE_NAME, Course.Type.TECNICO)
+        courseRepository.save(course)
+
+        courseExecution = new CourseExecution(course, ACRONYM, ACADEMIC_TERM, Course.Type.TECNICO)
+        courseExecutionRepository.save(courseExecution)
+
+        student = new User(USER_NAME, USER_USERNAME, 1, User.Role.STUDENT)
+        userRepository.save(student)
+
+        courseExecution.addUser(student)
+        student.addCourse(courseExecution)
+    }
+
+    def "create N_STUDENT_QUESTIONS questions with no image and one option"() {
+        given: "a questionDto"
+        def questionDto = new QuestionDto()
+        questionDto.setTitle(QUESTION_TITLE)
+        questionDto.setContent(QUESTION_CONTENT)
+        questionDto.setStatus(Question.Status.AVAILABLE.name())
+        and: 'a optionId'
+        def optionDto = new OptionDto()
+        optionDto.setContent(OPTION_CONTENT)
+        optionDto.setCorrect(true)
+        def options = new ArrayList<OptionDto>()
+        options.add(optionDto)
+        questionDto.setOptions(options)
+
+        when:
+        1.upto(N_STUDENT_QUESTIONS, {studentQuestionService.createStudentQuestion(course.getId(), questionDto, student.getId()); questionDto.setKey(null)})
+
+        then:
+        studentQuestionRepository.count() == N_STUDENT_QUESTIONS
+    }
+
+        @TestConfiguration
+    static class StudentQuestionServiceImplTestContextConfiguration {
+
+        @Bean
+        StudentQuestionService studentQuestionService() {
+            return new StudentQuestionService()
+        }
+    }
+}
