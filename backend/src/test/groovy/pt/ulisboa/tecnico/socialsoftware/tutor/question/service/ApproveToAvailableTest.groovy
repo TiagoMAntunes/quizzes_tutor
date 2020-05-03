bf91ea0cec77fd8712aac72ac351b490147ae8f8
apollee <mariabeatriz.dongamartins@gmail.com>
2020-05-03 21:23:03 +0100

    Defined tests cases for 'approved question becomes part of the available questions - Define test cases'. Issue #181

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/ApproveToAvailableTest.groovy
@@ -0,0 +1,23 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.service
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import spock.lang.Specification
+
+@DataJpaTest
+class ApproveToAvailableTest extends Specification{
+
+    def "a question is approved and then it is made available"(){
+        //the question becomes part of the available questions
+        expect: false
+    }
+
+    def "a question is rejected and then it is made available"(){
+        //an exception is thrown
+        expect: false
+    }
+
+    def "a question is in the pending status and then it is made available"(){
+        //an exception is thrown
+        expect: false
+    }
+}
