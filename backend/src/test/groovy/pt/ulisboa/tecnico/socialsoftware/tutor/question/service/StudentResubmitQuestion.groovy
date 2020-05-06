0c1651255b5f8078fd26651c9f208d41a2b53b84
pedro-lamego <pedrownlamego@hotmail.com>
2020-05-06 17:33:07 +0100

    Ppa:6.1 Tests defined #241

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/question/service/StudentResubmitQuestion.groovy
@@ -0,0 +1,26 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.question.service
+
+import spock.lang.Specification
+
+class StudentResubmitQuestion extends Specification {
+
+    def setup() {
+
+    }
+
+    def "student resubmit a rejected question"() {
+        expect: true;
+    }
+
+    def "student resubmit a approved question"() {
+        expect: true;
+    }
+
+    def "student resubmit a pending question"() {
+        expect: true;
+    }
+
+    def "teacher resubmit a rejected question"() {
+        expect: true;
+    }
+}
