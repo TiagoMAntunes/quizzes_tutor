b7fca35b65188a78ce75c5afb95804f35736e277
pedro-lamego <pedrownlamego@hotmail.com>
2020-05-07 19:37:16 +0100

    PPA:F8.1 Tests defined #272

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/user/domain/user/QuestionPrivacyTest.groovy
@@ -0,0 +1,18 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.user
+
+import spock.lang.Specification
+
+class QuestionPrivacyTest extends Specification {
+
+    def setup() {
+
+    }
+
+    def "Change question privacy to public"(){
+        expect: true;
+    }
+
+    def "Change question privacy to private"(){
+        expect: true;
+    }
+}
