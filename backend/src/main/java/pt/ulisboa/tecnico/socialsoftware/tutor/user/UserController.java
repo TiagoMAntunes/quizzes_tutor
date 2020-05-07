f341c4d2755f2c7163f3e20af509a5de3774f3a3
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2020-05-07 13:26:40 +0100

    Implemented web service and defined functionality test for tournament privacy closes #258 closes #259

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/user/UserController.java
@@ -0,0 +1,35 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage;
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException;
+
+import java.security.Principal;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PutMapping("/user/tournament/privacy")
+    @PreAuthorize("hasRole('ROLE_STUDENT')")
+    public ResponseEntity setTournamentPrivacy(@RequestParam boolean privacy, Principal principal) {
+        User user = (User) ((Authentication) principal).getPrincipal();
+
+        if (user == null) {
+            throw new TutorException(ErrorMessage.AUTHENTICATION_ERROR);
+        }
+
+        userService.setTournamentPrivacy(user.getId(), privacy);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
